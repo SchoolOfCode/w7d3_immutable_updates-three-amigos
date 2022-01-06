9ac55ac1c45877506165a0d024738d191f764776
troywebber <troywebb95@gmail.com>
2022-01-06 12:15:15 +0000

    array methods complete

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 // Arrays:
 
 // Immutably add an item to the end of an array
-export function addToEnd(array, item) {}
+export function addToEnd(array, item) {
+  return [...array, item];
+}
 
 // Immutably add an item to the beginning of an array
-export function addToBeginning(array, item) {}
+export function addToBeginning(array, item) {
+  return [item, ...array];
+}
 
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+  return [...array.slice(0, index), item, ...array.slice(index)];
+}
 
 // Immutably replace an item at a specific position/index within an array
-export function replaceItem(array, item, index) {}
+export function replaceItem(array, item, index) {
+  return [...array.slice(0, index), item, ...array.slice(index + 1)];
+}
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+}
 
 //Objects:
 
